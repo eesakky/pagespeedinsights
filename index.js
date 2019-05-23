50d25b86d06518dd13ad8be96dc10e4a7c293fca
Esakkimuthu E <esakki.er@gmail.com>
2019-05-23 22:46:11 +0530

    added url validation

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
 const { google } = require('googleapis');
+const isUrl = require('./lib/validateUrl.js');
+const fs = require('fs');
 
 module.exports = options => {
     const apiVersion = options.apiversion || 'v5';
-    const  getResult = google.pagespeedonline(apiVersion).pagespeedapi.runpagespeed(options)
+    const  initPageSpeed = google.pagespeedonline(apiVersion)
+    let collectedDataFromAPI = [];
 
-    console.log(getResult);
+    const psiData = options.urls.map( (url) => {
+        if (!isUrl(url)) {
+            return false;
+        }
 
-    getResult.then(function(result) {
-        console.log(result);
-    }).catch(function(error) {
-        console.log(error);
+        return new Promise((resolve, reject) => {
+          const getOptions = {
+            url: url,
+            key: options.key,
+            strategy: options.strategy
+          }
+
+          return resolve (
+            initPageSpeed.pagespeedapi.runpagespeed(getOptions)
+              .then((report) => {
+                collectedDataFromAPI.push(report.data);
+              })
+              .catch(error => {
+                console.error('Report failed : '+ error);
+              })
+          );
+        })
+    });
+
+    Promise.all(psiData).then(function() {
+        if (psiData.includes(false)) {
+            console.log(`\n Invalid url. Url must match the following regular expression: '(?i)http(s)?://.*' \n Please check your url list and try again \n`);
+            return;
+        };
+
+        const dataForJson = JSON.stringify(collectedDataFromAPI);
+        fs.writeFile('./report.json', dataForJson, 'utf8', (error) => { error != null ? console.log('Error' + error) : "" });
+        console.log(`\n Report has been generated successfully \n`);
     })
-    
-
 }
--- sample/test.js
+++
@@ -1,7 +0,0 @@
-const speed = require("./index.js");
-
-options = {
-    url: "https://www.google.co.in"
-}
-
-speed(options);
